9708edd921e41947e0b1a3b8538c47c4b807028e
lokesh <lokeshwark2006@gmail.com>
2026-02-18 11:11:47 +0530

    DSA-01

---
+++ Mac.java
@@ -0,0 +1,6 @@
+class Mac{
+
+public static void  main(String args[]){
+System.out.println("Good Morning");
+}
+}
---
+++ TernaryExample.java
@@ -0,0 +1,10 @@
+class TernaryExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int max = (a > b) ? a : b;
+
+        System.out.println("Maximum number is: " + max);
+    }
+}
